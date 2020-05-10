6fd24d7d6c7f2313950745fcad9982809471ea14
Ichiro Hiraide <hiraide@atama.plus>
2020-05-10 15:05:30 +0900

    added prettier along with eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import logo from './logo.svg';
 import style from './App.module.css';
 
-const App: FunctionComponent = () =>
-  (
-    <div className={style.app}>
-      <header className={style.appHeader}>
-        <img src={logo} className={style.appLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={style.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-
+const App: FunctionComponent = () => (
+  <div className={style.app}>
+    <header className={style.appHeader}>
+      <img src={logo} className={style.appLogo} alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className={style.appLink}
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
